89068e81f555d818c6253fc38444b94d6b91ebe0
Deepak <dp55954@gmail.com>
2026-01-23 17:01:04 +0530

    new program

---
+++ literals.py
@@ -0,0 +1,21 @@
+x=1
+print(x)
+print(type(x))
+y=3
+print(y<<2)
+print(y>>2)
+print(not(y))
+var = {(10,20),30,40}
+print(10 in var )
+name = "John"
+print(f"Hello, {name}!")
+print("Hello, {}".format(name))
+print(type(print("Hello, %s!" % name)))
+x=10
+# print()
+city="Hyderabad"
+state="Telangana"
+country="India"
+print(city, state, country)
+print(city, state, country, sep=',',end=" ")
+print(city, state, country, sep=',')
