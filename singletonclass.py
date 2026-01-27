ff3ad70d9b982dcaf76c449e61e2941672c4ac6b
Deepak <dp55954@gmail.com>
2026-01-27 18:23:34 +0530

    singletonclass

---
+++ singletonclass.py
@@ -0,0 +1,15 @@
+class Singletonclass:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            print('creating the object ')
+            cls._instance = super(Singletonclass,cls).__new__(cls)
+        return cls._instance
+    
+
+obj1= Singletonclass()
+print(obj1)
+obj2=Singletonclass()
+print(obj2)
+
+
