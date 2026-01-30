058c5b88e795efaf42c3003adc174134cef258aa
Deepak <dp55954@gmail.com>
2026-01-30 16:25:29 +0530

    collections

---
+++ colllection.py
@@ -0,0 +1,32 @@
+from  collections import *
+# s="asdfghjertdfgerdf"
+# print(Counter(s))
+
+# de=deque()
+# de.append(12)
+# de.append(12)
+# de.append(12)
+# de.append(12)
+# de.append(12)
+# de.append(12)
+# de.append(12)
+# print(de)
+# print(Counter(de))
+# de.popleft
+# print(len(de))
+
+# orr= OrderedDict()
+# orr['a']=1
+# print(orr)
+
+s=namedtuple('student',['name','age','class1'])
+s=s('deep',33,3)
+print(s)
+
+
+
+
+
+
+
+
